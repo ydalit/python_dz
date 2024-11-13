1d8426876a6ed6d7c9c0da35f1bd56e4ed264b3b
Артур Мичурин <rtrmcrn@gmail.com>
2024-11-14 02:20:25 +0500

    Add files via upload

---
+++ DZ_1/1.py
@@ -0,0 +1,20 @@
+def maximum(b,c):
+    a=int(input("Введите число a: "))
+    if a>0:
+        return max(a,b,c)
+    else:
+        return -1
+
+b=int(input("Введите число b: "))
+c=int(input("Введите число с: "))
+print(maximum(b,c))
+
+
+
+
+
+
+
+
+
+
